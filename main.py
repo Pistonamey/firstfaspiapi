7b2c837bc5853a4b2c4c844b79da34c4e04077b9
Pistonamey <ameyshinde11111@gmail.com>
2023-04-13 23:11:46 -0500

    First commit

---
+++ main.py
@@ -0,0 +1,40 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional, List
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = [
+    "https://deployreactfastapi.netlify.app"
+    "http://127.0.0.1.tiangolo.com",
+    "https://127.0.0.1.tiangolo.com",
+    "http://127.0.0.1",
+    "http://127.0.0.1:8000",
+    "http://127.0.0.1:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+class Person(BaseModel):
+    id: int
+    name: str
+
+
+DB: List[Person] = [
+    Person(id=1, name="Jamila", age=22),
+    Person(id=2, name="Alex", age=21)
+
+]
+
+
+@app.get("/api")
+async def root():
+    return DB
